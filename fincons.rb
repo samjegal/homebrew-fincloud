b25ee7d9cf0e11d33e2d994b489a7aaa6e7a4e51
samjegal <samjegal@gmail.com>
2020-02-29 22:01:57 +0900

    First commit

---
+++ fincons.rb
@@ -0,0 +1,12 @@
+class Fincons < Formula
+    desc 'VPN Connection tools for Financial Cloud'
+    homepage "https://www.github.com/samjegal/fincons"
+    url "https://github.com/samjegal/fincons/archive/v0.1.0.tar.gz"
+    sha256 "940ed609ec06b180f4def493bc515782086df922015d4b9f26004799fb2fe71d"
+    head "https://www.github.com/samjegal/fincons.git"
+    version "v0.1.0"
+
+    def install
+        bin.install "fincons"
+    end
+end
