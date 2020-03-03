8cb8744bff054e3781205e22ae3ee846538522dd
samjegal <samjegal@gmail.com>
2020-03-03 22:51:49 +0900

    information about fincons has changed

--- fincons.rb
+++ fincons.rb
@@ -1,8 +1,8 @@
 class Fincons < Formula
     desc 'VPN Connection tools for Financial Cloud'
     homepage "https://www.github.com/samjegal/fincons"
-    url "https://github.com/samjegal/fincons/archive/v0.1.0.tar.gz"
-    sha256 "940ed609ec06b180f4def493bc515782086df922015d4b9f26004799fb2fe71d"
+    url "https://github.com/samjegal/fincons/archive/v0.1.1.tar.gz"
+    sha256 "a9d0ff0de0f81d3e83ba47be4d1812a712296add1dc07153bfb7d83173478823"
     head "https://www.github.com/samjegal/fincons.git"
     version "v0.1.1"
 
