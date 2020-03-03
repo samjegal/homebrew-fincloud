91d41baba21882b719ab354f20101920b1991b1d
samjegal <samjegal@gmail.com>
2020-03-03 22:39:27 +0900

    Add depend package and change version

--- fincons.rb
+++ fincons.rb
@@ -4,7 +4,9 @@ class Fincons < Formula
     url "https://github.com/samjegal/fincons/archive/v0.1.0.tar.gz"
     sha256 "940ed609ec06b180f4def493bc515782086df922015d4b9f26004799fb2fe71d"
     head "https://www.github.com/samjegal/fincons.git"
-    version "v0.1.0"
+    version "v0.1.1"
+
+    depends_on "fzf"
 
     def install
         bin.install "fincons"
