06a4fa342ebd2b8dfabf850fa4a543128f48d97a
samjegal <samjegal@gmail.com>
2020-03-04 23:11:30 +0900

    Fix wrong finserv version

--- finserv.rb
+++ finserv.rb
@@ -1,7 +1,7 @@
 class Finserv < Formula
     desc 'Server SSL VPN connection for Financial Cloud'
     homepage "https://www.github.com/samjegal/finserv"
-    url "https://github.com/samjegal/finserv/archive/v0.1.1.tar.gz"
+    url "https://github.com/samjegal/finserv/archive/v0.1.0.tar.gz"
     sha256 "0ce40a228ed461c2955d136d82f606a7b8c023563859c80844e1a3f410ec6cec"
     head "https://www.github.com/samjegal/finserv.git"
     version "v0.1.0"
