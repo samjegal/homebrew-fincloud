badd6984fbaa723a4de9c9600f995c8d7ecb24df
samjegal <samjegal@gmail.com>
2020-04-21 22:24:07 +0900

    Add terraform provider for financial cloud

---
+++ terraform-provider-fincloud.rb
@@ -0,0 +1,15 @@
+class TerraformProviderFincloud < Formula
+    desc 'Terraform provider for Financial Cloud'
+    homepage "https://github.com/samjegal/terraform-provider-fincloud"
+    url "https://github.com/samjegal/terraform-provider-fincloud/releases/download/v1.0.0/terraform-provider-fincloud.osx.tar.gz"
+    sha256 "76771a3c45ce9dcfca40df3e251f67a24d0403308d4735ae1be199bfc1e14b63"
+    head "https://github.com/samjegal/terraform-provider-fincloud.git"
+    version "v1.0.0"
+
+    depends_on "terraform" >= "0.12.20"
+
+    def install
+        (var/"~/.terraform.d/plugins/darwin_amd64").mkpath
+        (var/"~/.terraform.d/plugins/darwin_amd64").install "terraform-provider-fincloud"
+    end
+end
