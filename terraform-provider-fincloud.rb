6a05c73ec23b3dd2722af2138195f3ca36ad46db
samjegal <samjegal@gmail.com>
2020-04-23 00:23:21 +0900

    Update release v1.0.1 terraform-provider-fincloud

--- terraform-provider-fincloud.rb
+++ terraform-provider-fincloud.rb
@@ -1,10 +1,10 @@
 class TerraformProviderFincloud < Formula
     desc 'Terraform provider for Financial Cloud'
     homepage "https://github.com/samjegal/terraform-provider-fincloud"
-    url "https://github.com/samjegal/terraform-provider-fincloud/releases/download/v1.0.0/terraform-provider-fincloud.osx.tar.gz"
-    sha256 "76771a3c45ce9dcfca40df3e251f67a24d0403308d4735ae1be199bfc1e14b63"
+    url "https://github.com/samjegal/terraform-provider-fincloud/releases/download/v1.0.1/terraform-provider-fincloud.osx.tar.gz"
+    sha256 "4bb60bccf2cd1c5c1f73d35e0e61f31c67621aadbac1ef3e642dd03bd20d88b3"
     head "https://github.com/samjegal/terraform-provider-fincloud.git"
-    version "v1.0.0"
+    version "v1.0.1"
 
     depends_on "terraform"
 
