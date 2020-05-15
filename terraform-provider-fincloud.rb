294268f8f660b18d89bded322370ea69775128ea
P.Elley <samjegal@gmail.com>
2020-05-15 15:34:44 +0900

    Update sha256 for terraform plugin v1.0.1

--- terraform-provider-fincloud.rb
+++ terraform-provider-fincloud.rb
@@ -2,7 +2,7 @@ class TerraformProviderFincloud < Formula
     desc 'Terraform provider for Financial Cloud'
     homepage "https://github.com/samjegal/terraform-provider-fincloud"
     url "https://github.com/samjegal/terraform-provider-fincloud/releases/download/v1.0.1/terraform-provider-fincloud.osx.tar.gz"
-    sha256 "4bb60bccf2cd1c5c1f73d35e0e61f31c67621aadbac1ef3e642dd03bd20d88b3"
+    sha256 "ed49f868284d556b5f6b7aeb6532873c53be10dbbc99c223bd8f03cc6f096ab3"
     head "https://github.com/samjegal/terraform-provider-fincloud.git"
     version "v1.0.1"
 
